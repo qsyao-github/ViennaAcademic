315df569ed849466d8a5e077834a2f03c31a174d
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2024-09-17 21:21:46 +0800

    Update omniparse.py

--- omniparse.py
+++ omniparse.py
@@ -2,22 +2,50 @@ import requests
 import base64
 from PIL import Image
 from io import BytesIO
+import subprocess
 #### api request ####
-url = 'http://0.0.0.0:8000/parse_document'
-file_path = './2408.10205v1.pdf'
+url = 'http://host.docker.internal:8000/parse_document'
+file_path = './2407.07061v2.pdf'
 def parseDocument(file_path):
+    url = 'http://localhost:8000/parse_document'
     with open(file_path, 'rb') as f:
         files = {'file': f}
         response = requests.post(url, files=files)
 
     result = response.json()
     text = result['text']
-    with open("./parse_results/test.md", "w") as f:
+    with open(f"{file_path}.md", "w") as f:
         f.write(text)
-
-
-    for raw in result['images']:
-        raw_decode = base64.b64decode(raw['image'])
-        image_name = raw['image_name']
-        Image.open(BytesIO(raw_decode)).save(f'./parse_results/{image_name}', 'PNG')
     return text
+def parseImage(file_path):
+    url = 'http://localhost:8000/parse_media/image'
+    with open(file_path, 'rb') as f:
+        files = {'file': f}
+        response = requests.post(url, files=files)
+
+    result = response.json()
+    text = result['text']
+    with open(f"{file_path}.md", "w") as f:
+        f.write(text)
+def parseVideo(file_path):
+    url= 'http://localhost:8000/parse_media/video'
+    with open(file_path, 'rb') as f:
+        files = {'file': f}
+        response = requests.post(url, files=files)
+    result = response.json()
+    text = result['text']
+    with open(f"{file_path}.md", "w") as f:
+        f.write(text)
+def parseAudio(file_path):
+    url= 'http://localhost:8000/parse_media/audio'
+    with open(file_path, 'rb') as f:
+        files = {'file': f}
+        response = requests.post(url, files=files)
+    result = response.json()
+    text = result['text']
+    with open(f"{file_path}.md", "w") as f:
+        f.write(text)
+def parseWebsites(webUrl):
+    result =subprocess.run(['curl','-X','POST','-H','"Content-Type: application/json"','-d',f'{{"url": "{webUrl}"}}','http://0.0.0.0:8000/parse_website'],capture_output=True,text=True,shell=True,check=True,encoding='utf-8')
+    return result.stdout
+print(parseDocument(file_path))
