d6c38aabb173fd0f8cdde8c34904b21c8111f5f7
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2024-09-24 22:04:09 +0800

    Update ipython.py

--- ipython.py
+++ ipython.py
@@ -1,13 +1,17 @@
 import subprocess
 import tempfile
 import os
+
+
 def execute_code(code):
-    with tempfile.NamedTemporaryFile(mode='w+', suffix='.py', delete=False) as tf:
+    with tempfile.NamedTemporaryFile(mode='w+', suffix='.py',
+                                     delete=False) as tf:
         temp_file_name = tf.name
         tf.write(code)
     try:
-        result=subprocess.check_output(['python', temp_file_name], stderr=subprocess.STDOUT)
-        return(result.decode('utf-8'))
+        result = subprocess.check_output(['python', temp_file_name],
+                                         stderr=subprocess.STDOUT)
+        return (result.decode('utf-8'))
     except subprocess.CalledProcessError as e:
         return e.output.decode('utf-8')
     finally:
