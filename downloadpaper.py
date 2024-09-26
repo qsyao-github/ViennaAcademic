e3d16cc83919df3b9ccb7c5fb263a79d3b7d987d
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2024-09-26 21:57:26 +0800

    Update downloadpaper.py

--- downloadpaper.py
+++ downloadpaper.py
@@ -1,25 +1,31 @@
-from scihub import SciHub
-from chat import chatbot
 from omniparse import parseDocument
 import os
 import arxiv
+import ollama
 
-sh = SciHub()
 
-
-def downloadDOIPaper(unknown):
-    paperbot = chatbot()
-    result = sh.download(f'{unknown}', path='./paper/temporaryPaper.pdf')
-    text = parseDocument('temporaryPaper.pdf')
-    title = paperbot.chat(f"下面是一篇论文的前255个字符，请返回该论文的标题：\n{text[:255]}")
-    os.rename('paper/temporaryPaper.pdf', f'paper/{title.strip()}.pdf')
+def title_generate(prompt):
+    answer = ""
+    stream = ollama.chat(
+        model='glm4',
+        messages=[{
+            'role': 'user',
+            'content': prompt
+        }],
+        stream=True,
+    )
+    for chunk in stream:
+        chunks = chunk['message']['content']
+        print(chunks, end='', flush=True)
+        answer += chunks
+    return answer
 
 
 def downloadArxivPaper(arxivID):
-    paperbot = chatbot()
     paper = next(arxiv.Client().results(arxiv.Search(id_list=[f"{arxivID}"])))
     # Download the PDF to a specified directory with a custom filename.
-    paper.download_pdf(dirpath="./paper", filename="temporaryPaper.pdf")
-    text = parseDocument('temporaryPaper.pdf')
-    title = paperbot.chat(f"下面是一篇论文的前255个字符，请返回该论文的标题：\n{text[:255]}")
-    os.rename('paper/temporaryPaper.pdf', f'paper/{title.strip()}.pdf')
+    paper.download_pdf(dirpath="paper", filename="temporaryPaper.pdf")
+    text = parseDocument('./paper/temporaryPaper.pdf')
+    title = title_generate(f"下面是论文节选，请返回该论文的标题：\n{text[:255]}")
+    os.rename('./paper/temporaryPaper.pdf', f'./paper/{title.strip()}.pdf')
+    os.remove('./paper/temporaryPaper.txt')
