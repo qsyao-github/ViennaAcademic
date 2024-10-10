cab40b4d5310e0f8170ce0e258792923765fcb92
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2024-10-10 09:24:40 +0800

    Update downloadpaper.py

--- downloadpaper.py
+++ downloadpaper.py
@@ -1,31 +1,33 @@
 from omniparse import parseDocument
 import os
 import arxiv
-import ollama
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-114514",
+                base_url="https://1919810.com/v1")
 
 
 def title_generate(prompt):
-    answer = ""
-    stream = ollama.chat(
-        model='glm4',
-        messages=[{
-            'role': 'user',
-            'content': prompt
-        }],
-        stream=True,
-    )
-    for chunk in stream:
-        chunks = chunk['message']['content']
-        print(chunks, end='', flush=True)
-        answer += chunks
-    return answer
+    response = client.chat.completions.create(model="gpt-4o-mini",
+                                              messages=[{
+                                                  "role": "user",
+                                                  "content": prompt
+                                              }])
+    return response.choices[0].message.content
 
 
 def downloadArxivPaper(arxivID):
-    paper = next(arxiv.Client().results(arxiv.Search(id_list=[f"{arxivID}"])))
-    # Download the PDF to a specified directory with a custom filename.
-    paper.download_pdf(dirpath="paper", filename="temporaryPaper.pdf")
-    text = parseDocument('./paper/temporaryPaper.pdf')
-    title = title_generate(f"下面是论文节选，请返回该论文的标题：\n{text[:255]}")
-    os.rename('./paper/temporaryPaper.pdf', f'./paper/{title.strip()}.pdf')
-    os.remove('./paper/temporaryPaper.txt')
+    if arxivID:
+        paper = next(arxiv.Client().results(
+            arxiv.Search(id_list=[f"{arxivID}"])))
+        # Download the PDF to a specified directory with a custom filename.
+        paper.download_pdf(dirpath="paper", filename="temporaryPaper.pdf")
+        print('startParsing')
+        text = parseDocument('./paper/temporaryPaper.pdf')
+        print('parsingDone')
+        title = title_generate(f"下面是论文节选，请提取论文标题，不要返回其它内容：\n{text[:255]}")
+        abstract = text[text.find('Abstract') + 9:][:text.find('\n')]
+        abstract = title_generate(f"将论文摘要译为中文，不要返回其它内容：\n{abstract}")
+        with open(f'knowledgeBase/{title.strip()}.md', mode='w') as f:
+            f.write(text)
+        os.rename('./paper/temporaryPaper.pdf', f'./paper/{title.strip()}.pdf')
