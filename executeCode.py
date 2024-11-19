c10f643705031d8c20323fd6c1ccaf1b5435e82e
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2024-11-19 22:40:05 +0800

    Update executeCode.py

--- executeCode.py
+++ executeCode.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 import shutil
-import time
 
 
 def execute_code(code):
@@ -14,23 +13,27 @@ def execute_code(code):
     if stdout:
         stdout = 'In[1]: ' + code + '\n' + stdout[stdout.find('In [1]: ') +
                                                   8:stdout.rfind('\nIn')]
+        outputList = stdout.split('\n')
+        outputList = [
+            line for line in outputList
+            if line[line.rfind(':') + 1:line.rfind('\n')] != ''
+        ]
+        stdout = '\n'.join(outputList)
     return stdout if stdout else '' + stderr if stderr else ''
 
 
-def manim_render(code):
-
-    start = code.find('def construct(self):')
-
+def manim_render(code, nowTime):
     code = "from manim import *\n" + code
-
-    os.chdir(r'C:\Users\15081\Desktop\glmacademic')
-
     if os.path.exists('media'):
         shutil.rmtree('media')
-
     with open('temp.py', 'w') as f:
         f.write(code)
     os.system(f'manim temp.py -ql')
-    os.chdir(r'C:/Users/15081/Desktop/glmacademic/media/videos/temp/480p15')
-    output_dir = [f for f in os.listdir() if f.endswith('.mp4')][0]
-    return output_dir
+    output = [
+        f for f in os.listdir(r'media/videos/temp/480p15/')
+        if f.endswith('.mp4')
+    ][0]
+    os.rename(r'media/videos/temp/480p15/' + output,
+              r'media/videos/temp/480p15/' + nowTime + '.mp4')
+    shutil.move(r'media/videos/temp/480p15/' + nowTime + '.mp4', os.getcwd())
+    return nowTime + '.mp4'
