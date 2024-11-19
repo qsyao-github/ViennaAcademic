e76ad66598f0211b436a0c5250c2d9af5efe565a
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2024-11-19 22:39:41 +0800

    Update downloadpaper.py

--- downloadpaper.py
+++ downloadpaper.py
@@ -1,10 +1,10 @@
-from omniparse import parseDocument
+from laoweiService import parseEverything
 import os
 import arxiv
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-114514",
-                base_url="https://1919810.com/v1")
+client = OpenAI(api_key="114514",
+                base_url="https://114514.com/v1")
 
 
 def title_generate(prompt):
@@ -23,7 +23,7 @@ def downloadArxivPaper(arxivID):
         # Download the PDF to a specified directory with a custom filename.
         paper.download_pdf(dirpath="paper", filename="temporaryPaper.pdf")
         print('startParsing')
-        text = parseDocument('./paper/temporaryPaper.pdf')
+        text = parseEverything('./paper/temporaryPaper.pdf')
         print('parsingDone')
         title = title_generate(f"下面是论文节选，请提取论文标题，不要返回其它内容：\n{text[:255]}")
         abstract = text[text.find('Abstract') + 9:][:text.find('\n')]
