efdd477296ea1ed5eb162e921b310ecf73e94023
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2024-12-27 16:49:09 +0800

    Update doclingparse.py

--- doclingparse.py
+++ doclingparse.py
@@ -1,17 +1,45 @@
 from docling.document_converter import DocumentConverter
 from docling.datamodel.base_models import InputFormat
-from docling.document_converter import DocumentConverter, PdfFormatOption
-from docling.datamodel.pipeline_options import PdfPipelineOptions, TableFormerMode
+from docling.document_converter import (DocumentConverter, PdfFormatOption,
+                                        WordFormatOption, ImageFormatOption)
+from docling.pipeline.simple_pipeline import SimplePipeline
+from docling.datamodel.pipeline_options import PdfPipelineOptions, RapidOcrOptions
+from docling.backend.docling_parse_v2_backend import DoclingParseV2DocumentBackend
+from docling.datamodel.document import ConversionResult
+from rapidocr_onnxruntime import RapidOCR
 
-pipeline_options = PdfPipelineOptions(do_table_structure=True)
-pipeline_options.table_structure_options.do_cell_matching = False  # uses text cells predicted from table structure model
-pipeline_options.table_structure_options.mode = TableFormerMode.ACCURATE
-doc_converter = DocumentConverter(
-    format_options={
-        InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
-    }
+engine = RapidOCR()
+# Docling Parse with Tesseract
+# ----------------------
+pipeline_options = PdfPipelineOptions()
+pipeline_options.do_ocr = True
+pipeline_options.do_table_structure = True
+pipeline_options.table_structure_options.do_cell_matching = True
+pipeline_options.ocr_options = RapidOcrOptions()
+
+doc_converter = (
+    DocumentConverter(  # all of the below is optional, has internal defaults.
+        allowed_formats=[
+            InputFormat.PDF,
+            InputFormat.IMAGE,
+            InputFormat.DOCX,
+            InputFormat.HTML,
+            InputFormat.PPTX,
+        ],  # whitelist formats, non-matching files are ignored.
+        format_options={
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_options=pipeline_options, # pipeline options go here.
+                backend=DoclingParseV2DocumentBackend # optional: pick an alternative backend
+            ),
+            InputFormat.DOCX: WordFormatOption(
+                pipeline_cls=SimplePipeline # default for office formats and HTML
+            ),
+            InputFormat.IMAGE: ImageFormatOption(pipeline_options=pipeline_options)
+        },
+    )
 )
-def parseEverything(source):
-    converter = doc_converter
-    result = converter.convert(source)
-    return result.document.export_to_markdown()
+
+
+def parse(file):
+    conv_result: ConversionResult = doc_converter.convert(file)
+    return conv_result.document.export_to_markdown()
