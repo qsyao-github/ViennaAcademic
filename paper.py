a30ed7af210aebaa7fc2e9e54b5b4eb6abed1c3c
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-01-21 15:23:40 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -21,11 +21,27 @@ def attach(file):
 
 def chunk(content,
           separators=[
-              "\n#", "\n=", r"\section", r"\subsection", r"\subsubsection",
-              r"\paragraph", r"\subparagraph"
+              "\n# ",
+              "\n## ",
+              "\n### ",
+              "\n#### ",
+              "\n##### ",
+              "\n###### ",
+              "\n= ",
+              "\n== ",
+              "\n=== ",
+              "\n==== ",
+              "\n===== ",
+              "\n====== ",
+              r"\section",
+              r"\subsection",
+              r"\subsubsection",
+              r"\paragraph",
+              r"\subparagraph",
           ]):
-    pattern = '|'.join(map(regex.escape, separators))
-    return regex.split(pattern, content)
+    pattern = '|'.join([f"({sep})" for sep in separators])
+    temp_list = regex.split(pattern, content)
+    return [i for i in temp_list if i is not None]
 
 
 def readPaper(file_Path):
@@ -36,20 +52,41 @@ def readPaper(file_Path):
 4. 这篇论文的方法是怎样的？它如何突破了这个核心困难？
 5. 论文是如何设计实验验证的？有什么值得学习之处？
 6. 这篇论文有什么局限？'''
-    answer=""
+    answer = ""
     stream = client2.chat.completions.create(model="glm-4-flash",
-                                               messages=[{
-                                                   "role": "user",
-                                                   "content": prompt
-                                               }],stream=True)
+                                             messages=[{
+                                                 "role": "user",
+                                                 "content": prompt
+                                             }],
+                                             stream=True)
     for chunk in stream:
-        answer+=chunk.choices[0].delta.content or ""
+        answer += chunk.choices[0].delta.content or ""
         yield (f"解读论文{file_Path}", answer)
 
 
-def translation(string, extraPrompt):
+def translation(string,
+                extraPrompt,
+                separators=[
+              "\n# ",
+              "\n## ",
+              "\n### ",
+              "\n#### ",
+              "\n##### ",
+              "\n###### ",
+              "\n= ",
+              "\n== ",
+              "\n=== ",
+              "\n==== ",
+              "\n===== ",
+              "\n====== ",
+              r"\section",
+              r"\subsection",
+              r"\subsubsection",
+              r"\paragraph",
+              r"\subparagraph",
+          ]):
     judge = string.lstrip().rstrip()
-    if judge != '\n' and judge != '':
+    if judge != '\n' and judge != '' and string not in separators:
         translation = client2.chat.completions.create(
             model="glm-4-flash",
             messages=[{
@@ -75,9 +112,10 @@ def translatePapertoChinese(file_Path):
             content = f.read()
         return content
     content = chunk(attach(file_Path))
+    input(str(content))
     length = len(content)
     threads = []
-    result_list = [None] * length
+    result_list = [""] * length
     if length >= 64:
         chunk_size = length // 64
         for i in range(64):
@@ -88,11 +126,6 @@ def translatePapertoChinese(file_Path):
                                        result_list, "将论文译为中文，仅需翻译，不作说明"))
             threads.append(t)
             t.start()
-        for t in threads:
-            t.join()
-        with open(f'paper/{file_Path[:file_Path.rfind(".")]}Chi.md',
-                  mode='w') as f:
-            f.write('\n'.join(result_list))
     else:
         for i in range(length):
             t = threading.Thread(target=process_part_of_list,
@@ -100,12 +133,14 @@ def translatePapertoChinese(file_Path):
                                        "将论文译为中文，仅需翻译，不作说明"))
             threads.append(t)
             t.start()
-        for t in threads:
-            t.join()
-        with open(f'paper/{file_Path[:file_Path.rfind(".")]}Chi.md',
-                  mode='w') as f:
-            f.write('\n'.join(result_list))
-    return ("将论文译为中文", '\n'.join(result_list))
+    while any(thread.is_alive() for thread in threads):
+        yield ("将论文译为中文", ''.join(result_list))
+    for t in threads:
+        t.join()
+    with open(f'knowledgeBase/{file_Path[:file_Path.rfind(".")]}Chi.md',
+              mode='w') as f:
+        f.write(''.join(result_list))
+    yield ("将论文译为中文", ''.join(result_list))
 
 
 def translatePapertoEnglish(file_Path):
@@ -118,7 +153,7 @@ def translatePapertoEnglish(file_Path):
     content = chunk(attach(file_Path))
     length = len(content)
     threads = []
-    result_list = [None] * length
+    result_list = [""] * length
     if length >= 64:
         chunk_size = length // 64
         for i in range(64):
@@ -131,7 +166,7 @@ def translatePapertoEnglish(file_Path):
             t.start()
         for t in threads:
             t.join()
-        with open(f'paper/{file_Path[:file_Path.rfind(".")]}Eng.md',
+        with open(f'knowledgeBase/{file_Path[:file_Path.rfind(".")]}Eng.md',
                   mode='w') as f:
             f.write('\n'.join(result_list))
     else:
@@ -143,7 +178,7 @@ def translatePapertoEnglish(file_Path):
             t.start()
         for t in threads:
             t.join()
-        with open(f'paper/{file_Path[:file_Path.rfind(".")]}Eng.md',
+        with open(f'knowledgeBase/{file_Path[:file_Path.rfind(".")]}Eng.md',
                   mode='w') as f:
             f.write('\n'.join(result_list))
     return ("将论文译为英文", '\n'.join(result_list))
@@ -159,7 +194,7 @@ def polishPaper(file_Path):
     content = chunk(attach(file_Path))
     length = len(content)
     threads = []
-    result_list = [None] * length
+    result_list = [""] * length
     if length >= 64:
         chunk_size = length // 64
         for i in range(64):
@@ -173,7 +208,7 @@ def polishPaper(file_Path):
             t.start()
         for t in threads:
             t.join()
-        with open(f'paper/{file_Path[:file_Path.rfind(".")]}Pol.md',
+        with open(f'knowledgeBase/{file_Path[:file_Path.rfind(".")]}Pol.md',
                   mode='w') as f:
             f.write('\n'.join(result_list))
     else:
@@ -185,7 +220,7 @@ def polishPaper(file_Path):
             t.start()
         for t in threads:
             t.join()
-        with open(f'paper/{file_Path[:file_Path.rfind(".")]}Pol.md',
+        with open(f'knowledgeBase/{file_Path[:file_Path.rfind(".")]}Pol.md',
                   mode='w') as f:
             f.write('\n'.join(result_list))
     return ("润色论文", '\n'.join(result_list))
