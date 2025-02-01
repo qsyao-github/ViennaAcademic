43302d3bfa2ca290a8fe61bed5d4aad9cb24e108
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-02-01 11:33:56 +0800

    Update perplexica.py

--- perplexica.py
+++ perplexica.py
@@ -1,9 +1,11 @@
 import requests
 from modelclient import client1API_KEY, client1BASE_URL
-proxies = {"http":"http://localhost:7890","https":"http://localhost:7890"}
 
-def webSearch(query):
-    jsonquery = {
+proxies = {"http": "http://localhost:7890", "https": "http://localhost:7890"}
+
+
+def constructQuery(query, focusMode):
+    return {
         "chatModel": {
             "provider": "custom_openai",
             "model": "gpt-4o-mini",
@@ -15,84 +17,43 @@ def webSearch(query):
             "model": "mxbai-embed-large:latest"
         },
         "optimizationMode": "balanced",
-        "focusMode": "webSearch",
+        "focusMode": focusMode,
         "query": query
     }
-    response = requests.post('http://127.0.0.1:3001/api/search', json=jsonquery, proxies=proxies)
+
+
+def getResult(jsonquery):
+    response = requests.post('http://127.0.0.1:3001/api/parse',
+                             json=jsonquery,
+                             proxies=proxies)
     message = response.json()['message']
     sourcesList = response.json()['sources']
     referenceList = [
-        f'[{i+1}] [{sourcesList[i]["metadata"]["title"]}]({sourcesList[i]["metadata"]["url"]})'
-        for i in range(len(sourcesList))
+        f'[{i+1}] [{source["metadata"]["title"]}]({source["metadata"]["url"]})'
+        for i, source in enumerate(sourcesList)
     ]
-    return message + '\n\n参考文献\n\n' + '\n\n'.join(referenceList)
+    return message, referenceList, sourcesList
+
+
+def webSearch(query):
+    jsonquery = constructQuery(query, "webSearch")
+    message, referenceList, _ = getResult(jsonquery)
+    return (f"请搜索{query}",
+            message + '\n\n参考文献\n\n' + '\n\n'.join(referenceList))
 
 
 def academicSearch(query):
-    jsonquery = {
-        "chatModel": {
-            "provider": "custom_openai",
-            "model": "gpt-4o-mini",
-            "customOpenAIBaseURL": client1BASE_URL,
-            "customOpenAIKey": client1API_KEY
-        },
-        "embeddingModel": {
-            "provider": "ollama",
-            "model": "mxbai-embed-large:latest"
-        },
-        "optimizationMode": "balanced",
-        "focusMode": "academicSearch",
-        "query": query
-    }
-    response = requests.post('http://127.0.0.1:3001/api/search', json=jsonquery, proxies=proxies)
-    message = response.json()['message']
-    sourcesList = response.json()['sources']
-    referenceList = [
-        f'[{i+1}] [{sourcesList[i]["metadata"]["title"]}]({sourcesList[i]["metadata"]["url"]})'
-        for i in range(len(sourcesList))
-    ]
+    jsonquery = constructQuery(query, "academicSearch")
+    message, referenceList, _ = getResult(jsonquery)
     return ("查找" + query + "有关论文",
             message + '\n\n参考文献\n\n' + '\n\n'.join(referenceList))
 
 
 def stormSearch(query):
-    jsonquery = {
-        "chatModel": {
-            "provider": "custom_openai",
-            "model": "gpt-4o-mini",
-            "customOpenAIBaseURL": client1BASE_URL,
-            "customOpenAIKey": client1API_KEY
-        },
-        "embeddingModel": {
-            "provider": "ollama",
-            "model": "mxbai-embed-large:latest"
-        },
-        "optimizationMode": "balanced",
-        "focusMode": "academicSearch",
-        "query": query
-    }
-    response = requests.post('http://127.0.0.1:3001/api/search', json=jsonquery, proxies=proxies)
-    message1 = response.json()['message']
-    sourcesList1 = response.json()['sources']
-    jsonquery = {
-        "chatModel": {
-            "provider": "custom_openai",
-            "model": "gpt-4o-mini",
-            "customOpenAIBaseURL": client1BASE_URL,
-            "customOpenAIKey": client1API_KEY
-        },
-        "embeddingModel": {
-            "provider": "ollama",
-            "model": "mxbai-embed-large:latest"
-        },
-        "optimizationMode": "balanced",
-        "focusMode": "webSearch",
-        "query": query
-    }
-    response = requests.post('http://127.0.0.1:3001/api/search', json=jsonquery, proxies=proxies)
-    message2 = response.json()['message']
-    sourcesList2 = response.json()['sources']
+    jsonquery = constructQuery(query, "academicSearch")
+    message1, _, sourcesList1 = getResult(jsonquery)
+    jsonquery = constructQuery(query, "webSearch")
+    message2, _, sourcesList2 = getResult(jsonquery)
     message = message1 + '\n\n' + message2
     sourcesList = sourcesList1 + sourcesList2
     return message, sourcesList
-
