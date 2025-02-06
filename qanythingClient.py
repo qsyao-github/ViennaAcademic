2a1f4f809da160f757673a9651e7ed0b99549deb
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-02-06 12:33:07 +0800

    Update qanythingClient.py

--- qanythingClient.py
+++ qanythingClient.py
@@ -1,20 +1,11 @@
 from gradio_client import Client
 
+client = Client("http://localhost:7861/")
 
 def update():
-    client = Client("http://localhost:7862/")
-    result = client.predict(
-            api_name="/update"
-    )
+    _ = client.predict(api_name="/update")
 
 
 def qanything_fetch(query):
-    client = Client("http://localhost:7862/")
-    result = client.predict(
-            query=query,
-            api_name="/get_response"
-    )
+    result = client.predict(query=query, api_name="/get_response")
     return result
-
-
-print(qanything_fetch("SSL算法如何从大量未标记数据中学习有判别力的特征？"))
