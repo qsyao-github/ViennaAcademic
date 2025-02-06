b8f0e96de95e2ae14227f650b3d75cab6e174e97
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-02-06 12:33:51 +0800

    Update Drission.py

--- Drission.py
+++ Drission.py
@@ -1,11 +1,29 @@
 from DrissionPage import Chromium, ChromiumOptions
-
+import re
 co = ChromiumOptions().headless(True)
 co = co.set_argument('--no-sandbox')
 browser = Chromium(co)
 tab = browser.latest_tab
 
+def isChinese(query):
+    return bool(re.search(r'[\u4e00-\u9fff]', query))
+
 def get_html(url):
     tab.get(url, timeout=10)
     with open('temp.html', 'w') as f:
         f.write(tab.html)
+
+
+def get_wolfram(query):
+    finalAnswer = []
+    tab.get(f'https://www.wolframalpha.com/input?i={query}&lang=zh')
+    answer_head = tab.ele('xpath:/html/body/div/div/main/main/div[2]/div/div[2]/section')
+    answers = answer_head.eles("@|tag()=h2@|tag()=img")
+    for answer in answers:
+        if answer.tag == 'h2':
+            temp = '# '+answer.ele('xpath:/span').text
+        elif answer.tag == 'img':
+            temp = answer.attr('alt')
+        if temp and '图片' not in temp:
+            finalAnswer.append(temp)
+    return '\n'.join(finalAnswer)
