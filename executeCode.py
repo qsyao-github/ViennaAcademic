2d4072720308defd98f77f3c417ef18587eb0f33
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-02-28 21:08:50 +0800

    Update executeCode.py

--- executeCode.py
+++ executeCode.py
@@ -1,7 +1,8 @@
 import subprocess
-import os
-import shutil
 import re
+
+EXTRACT_MAIN_PATTERN = re.compile(r'In \[1\]:\s*(.*?)\nIn', re.DOTALL)
+CLEAN_LINE_PATTERN = re.compile(r'^In \[\d+\]:|^Out\[\d+\]:')
 
 
 def execute_code(code):
@@ -12,32 +13,16 @@ def execute_code(code):
                                text=True)
     stdout, stderr = process.communicate(code)
     if stdout:
-        stdout = '```python\n' + code + '\n```\n```output\n' + stdout[
-            stdout.find('In [1]: ') + 8:stdout.rfind('\nIn')] + '\n```'
+        stdout = f'```python\n{code}\n```\n```output\n{EXTRACT_MAIN_PATTERN.search(stdout).group(1)}\n```'
         lines = stdout.split('\n')
         outputList = []
         for line in lines:
-            cleanedLine = re.sub(r'^In \[\d+\]:|^Out\[\d+\]:', '', line.strip()).strip()
+            cleanedLine = CLEAN_LINE_PATTERN.sub('', line.strip()).strip()
             if cleanedLine:
                 outputList.append(cleanedLine)
         stdout = '\n'.join(outputList)
-    return stdout if stdout else '' + f'```error\n{stderr}\n```' if stderr else ''
+    return (stdout if stdout else '') + (f'```error\n{stderr}\n```'
+                                         if stderr else '')
 
-
-outputFolder = r'media/videos/temp/480p15/'
-def manim_render(code, nowTime):
-    try:
-        if '```' in code:
-            code = code[code.find('```python') + 10:code.rfind('```')]
-        if os.path.exists('media'):
-            shutil.rmtree('media')
-        with open('temp.py', 'w') as f:
-            f.write("from manim import *\n" + code)
-        subprocess.run(['manim', 'temp.py', '-ql'], check=True)
-        output = [f for f in os.listdir(outputFolder) if f.endswith('.mp4')][0]
-        new_output_path = os.path.join(outputFolder, f'{nowTime}.mp4')
-        os.rename(os.path.join(outputFolder, output), new_output_path)
-        shutil.move(new_output_path, os.getcwd())
-        return f'```python\n{code}\n```'
-    except:
-        return 'gpt生成代码有误，请重试'
+def insert_python(code):
+    return f'\n\n{execute_code(code)}\n'
