732fab177b77eb0888db8741d243e8baeab6b32a
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-03 15:24:24 +0800

    Update and rename doclingparse.py to docling_parser.py

---
+++ docling_parser.py
@@ -0,0 +1,72 @@
+from docling.datamodel.base_models import InputFormat
+from docling.document_converter import (
+    DocumentConverter,
+    PdfFormatOption,
+    WordFormatOption,
+    ImageFormatOption,
+)
+from docling.pipeline.simple_pipeline import SimplePipeline
+from docling.datamodel.pipeline_options import (
+    PdfPipelineOptions,
+    RapidOcrOptions,
+    TableFormerMode,
+    AcceleratorDevice,
+    AcceleratorOptions,
+)
+from docling.backend.docling_parse_v2_backend import DoclingParseV2DocumentBackend
+from docling.datamodel.document import ConversionResult
+from rapidocr_onnxruntime import RapidOCR
+import re
+
+engine = RapidOCR()
+pipeline_options = PdfPipelineOptions(artifacts_path="/home/laowei/model/docling")
+pipeline_options.do_ocr = True
+pipeline_options.accelerator_options = AcceleratorOptions(
+    num_threads=12, device=AcceleratorDevice.CPU
+)
+pipeline_options.do_table_structure = True
+pipeline_options.table_structure_options.do_cell_matching = True
+pipeline_options.ocr_options = RapidOcrOptions()
+pipeline_options.table_structure_options.mode = TableFormerMode.ACCURATE
+
+doc_converter = DocumentConverter(  # all of the below is optional, has internal defaults.
+    allowed_formats=[
+        InputFormat.PDF,
+        InputFormat.IMAGE,
+        InputFormat.DOCX,
+        InputFormat.HTML,
+        InputFormat.PPTX,
+    ],  # whitelist formats, non-matching files are ignored.
+    format_options={
+        InputFormat.PDF: PdfFormatOption(
+            pipeline_options=pipeline_options,  # pipeline options go here.
+            backend=DoclingParseV2DocumentBackend,  # optional: pick an alternative backend
+        ),
+        InputFormat.DOCX: WordFormatOption(
+            pipeline_cls=SimplePipeline  # default for office formats and HTML
+        ),
+        InputFormat.IMAGE: ImageFormatOption(pipeline_options=pipeline_options),
+    },
+)
+
+
+def parse_everything(file_or_url: str) -> str:
+    conv_result: ConversionResult = doc_converter.convert(file_or_url)
+    result = conv_result.document.export_to_markdown()
+    return result.replace("<!-- image -->", "").strip()
+
+
+ARXIV_STRIP_PATTERN = re.compile(
+    r"(.*?)(?=Generated  on \w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4} by)", re.DOTALL
+)
+
+
+def parse_arxiv(url: str) -> str:
+    try:
+        text = parse_everything(url)
+        match = ARXIV_STRIP_PATTERN.search(text)
+        if match is not None:
+            return match.group(1).strip()
+    except:
+        pass
+    return ""
