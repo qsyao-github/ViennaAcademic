1c8c04c0fb34b689a01d7a71986ef9c39a913481
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-04 11:55:57 +0800

    Update bce_inference.py

--- bce_inference.py
+++ bce_inference.py
@@ -10,11 +10,16 @@ from langchain.retrievers import ContextualCompressionRetriever
 import os
 
 reranker = CustomCompressor(10)
-text_splitter = RecursiveCharacterTextSplitter(separators=[r"\n{2,}", r"\n+"],
-                                               is_separator_regex=True,
-                                               chunk_overlap=16,
-                                               chunk_size=512)
-check_chars = ['。', '！', '？', '.', '!', '?']
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=[
+    r"\n{2,}", r"\n+", r"\s{2,}", r"(?<=[.!?;])\s+|(?<=[。？！；])",
+    r"(?<=[,])\s+|(?<=[，])", r"\s+"
+],
+    is_separator_regex=True,
+    chunk_overlap=16,
+    chunk_size=512,
+)
+check_chars = ["。", "！", "？", ".", "!", "?"]
 
 
 def get_document(file: str) -> List[Document]:
@@ -29,34 +34,38 @@ def get_retriever(file: str) -> FAISS:
         retriever = FAISS.from_documents(
             total_texts,
             bce_embedding_base,
-            distance_strategy=DistanceStrategy.MAX_INNER_PRODUCT)
+            distance_strategy=DistanceStrategy.MAX_INNER_PRODUCT,
+        )
         return retriever
 
 
 def save_retriever(file: str, current_dir: str) -> None:
-    try:
-        retriever = get_retriever(f'{current_dir}/knowledgeBase/{file}.md')
-        if retriever is not None:
-            retriever.save_local(f'{current_dir}/retrievers', file)
-    except:
-        pass
+    # try:
+    retriever = get_retriever(f"{current_dir}/knowledgeBase/{file}.md")
+    if retriever is not None:
+        retriever.save_local(f"{current_dir}/retrievers", file)
+    # except:
+        # pass
 
 
 def remove_retriever(file: str, current_dir: str) -> None:
     try:
-        os.remove(f'{current_dir}/retrievers/{file}.pkl')
-        os.remove(f'{current_dir}/retrievers/{file}.faiss')
+        os.remove(f"{current_dir}/retrievers/{file}.pkl")
+        os.remove(f"{current_dir}/retrievers/{file}.faiss")
     except:
         pass
 
 
 def update(current_dir: str) -> None:
+    print(current_dir)
     knowledgeBase = set(
-        os.path.splitext(file)[0]
-        for file in os.listdir(f'{current_dir}/knowledgeBase'))
+        os.path.splitext(file)[0] for file in os.listdir(f"{current_dir}/knowledgeBase")
+    )
     retrievers = set(
-        os.path.splitext(file)[0]
-        for file in os.listdir(f'{current_dir}/retrievers'))
+        os.path.splitext(file)[0] for file in os.listdir(f"{current_dir}/retrievers")
+    )
+    print(knowledgeBase)
+    print(retrievers)
     for file in knowledgeBase - retrievers:
         save_retriever(file, current_dir)
     for file in retrievers - knowledgeBase:
@@ -64,27 +73,29 @@ def update(current_dir: str) -> None:
 
 
 def merge_retrievers(current_dir: str) -> ContextualCompressionRetriever:
-    retriever_directory = f'{current_dir}/retrievers'
+    retriever_directory = f"{current_dir}/retrievers"
     retrievers = [
-        FAISS.load_local(retriever_directory,
-                         bce_embedding_base,
-                         file,
-                         distance_strategy=DistanceStrategy.MAX_INNER_PRODUCT,
-                         allow_dangerous_deserialization=True) for file in set(
-                             os.path.splitext(file)[0]
-                             for file in os.listdir(retriever_directory))
+        FAISS.load_local(
+            retriever_directory,
+            bce_embedding_base,
+            file,
+            distance_strategy=DistanceStrategy.MAX_INNER_PRODUCT,
+            allow_dangerous_deserialization=True,
+        )
+        for file in set(
+            os.path.splitext(file)[0] for file in os.listdir(retriever_directory)
+        )
     ]
     base_retriever = retrievers.pop()
     for retriever in retrievers:
         base_retriever.merge_from(retriever)
-    base_retriever = base_retriever.as_retriever(search_type="similarity",
-                                                 search_kwargs={
-                                                     "score_threshold": 0.35,
-                                                     "k": 100,
-                                                     "fetch-k": 100
-                                                 })
+    base_retriever = base_retriever.as_retriever(
+        search_type="similarity",
+        search_kwargs={"score_threshold": 0.35, "k": 100, "fetch-k": 100},
+    )
     compression_retriever = ContextualCompressionRetriever(
-        base_compressor=reranker, base_retriever=base_retriever)
+        base_compressor=reranker, base_retriever=base_retriever
+    )
     return compression_retriever
 
 
@@ -95,12 +106,17 @@ def get_response(query: str, current_dir: str) -> str:
     for document in response:
         indicator = "Source: "
         content = document.page_content.strip()
-        if content and '#' not in content and '\n' not in content and any(char in content for char in
-                check_chars) and document.metadata["relevance_score"] >= 0.35:
+        if (
+            content
+            and "#" not in content
+            and "\n" not in content
+            and any(char in content for char in check_chars)
+            and document.metadata["relevance_score"] >= 0.35
+        ):
             source = os.path.basename(document.metadata["source"])
-            if source.startswith('STORMtemp'):
+            if source.startswith("STORMtemp"):
                 source = source[9:]
                 indicator = "Number: "
             source = os.path.splitext(source)[0]
-            text_response.append(f'{content} [{indicator}{source}]')
-    return '\n\n'.join(text_response)
+            text_response.append(f"{content} [{indicator}{source}]")
+    return "\n\n".join(text_response)
