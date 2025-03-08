8cef82b848b5646e033ab022b78d16f92971cab6
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-08 17:59:14 +0800

    Update wolfram.py

--- wolfram.py
+++ wolfram.py
@@ -1,8 +1,45 @@
-from drission import get_wolfram
+import json
+import asyncio
+from crawl4ai import AsyncWebCrawler, CrawlerRunConfig
+from crawl4ai.extraction_strategy import JsonXPathExtractionStrategy
+
+
+async def get_wolfram(query: str) -> str:
+    schema = {
+        "name": "wolfram",
+        "baseSelector": '//section[@tabindex="0"]',
+        "fields": [
+            {"name": "title", "selector": ".//span", "type": "text"},
+            {
+                "name": "content",
+                "selector": ".//img[@alt]",
+                "type": "attribute",
+                "attribute": "alt",
+            },
+        ],
+    }
+    config = CrawlerRunConfig(
+        extraction_strategy=JsonXPathExtractionStrategy(schema, verbose=True),
+        wait_for="div.sc-a1dd50ea-0.LChfQ",
+    )
+    url = f"https://www.wolframalpha.com/input?i={query}&lang=zh"
+    async with AsyncWebCrawler(verbose=True) as crawler:
+        result = await crawler.arun(url, config)
+        if not result.success:
+            return
+        print(result.extracted_content)
+        data = json.loads(result.extracted_content)
+        chunk_result = []
+        for item in data:
+            if (title := item.get("title", "")) and title != "图形":
+                chunk_result.append(f"# {title}")
+            if (content := item.get("content", "")) != "图形":
+                chunk_result.append(content)
+        return "\n".join(chunk_result)
 
 
 def attach_hints(query: str) -> str:
-    hints = get_wolfram(query).strip()
+    hints = asyncio.run(get_wolfram(query))
     if hints:
         query = f"Wolframalpha提示：\n```\n{hints}\n```\n{query}"
     return query
