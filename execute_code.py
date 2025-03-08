4d8e915c92b8e9755bf9fe4df72c2e385e438ccb
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-08 23:01:35 +0800

    Update execute_code.py

--- execute_code.py
+++ execute_code.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+from langchain_core.tools import tool
 
 CLEAN_LINE_PATTERN = re.compile(r"^In \[\d+\]:\s*Out\[\d+\]:\s*")
 
@@ -12,20 +13,19 @@ def execute_code(code: str) -> str:
         stderr=subprocess.PIPE,
         text=True,
     )
-    stdout_output, stderr_output = process.communicate(code)
+    stdout_output, _ = process.communicate(code)
 
     output_blocks = []
 
-    if stdout_output:
-        extracted_output_list = [
-            line for line in stdout_output.split("\n") if "Out" in line
-        ]
-        cleaned_lines = [
-            CLEAN_LINE_PATTERN.sub("", line.strip()).strip()
-            for line in extracted_output_list
-        ]
-        cleaned_lines = [line for line in cleaned_lines if line]
-        cleaned_output = "\n".join(cleaned_lines)
+    if (index := stdout_output.find("In [1]")) != -1:
+        stdout_output = stdout_output[index:stdout_output.rfind("In [")]
+        splitted_output = stdout_output.split("\n")
+        for i, line in enumerate(splitted_output):
+            if not line[line.rfind(':')+1:].strip():
+                splitted_output[i] = ''
+            elif 'Out' in line:
+                splitted_output[i] = line[line.find(':')+2:]
+        cleaned_output = '\n'.join([line for line in splitted_output if line])
 
         code_block = f"```python\n{code}\n```"
         output_blocks.append(code_block)
@@ -33,8 +33,25 @@ def execute_code(code: str) -> str:
         if cleaned_output:
             output_blocks.append(f"```\n{cleaned_output}\n```")
 
-    error_block = ""
-    if stderr_output:
-        error_block = f"```error\n{stderr_output}\n```"
+    return f"\n\n{'\n'.join(output_blocks)}\n"
 
-    return f"\n\n{'\n'.join(output_blocks + [error_block]) if error_block else '\n'.join(output_blocks)}\n"
+@tool
+def python(code: str) -> str:
+    """Ipython cell. Use numpy, scipy, sympy for numeric and symbolic computation."""
+    process = subprocess.Popen(
+        ["ipython"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    stdout_output, _ = process.communicate(code)
+    if (index := stdout_output.find("In [1]")) != -1:
+        stdout_output = stdout_output[index:stdout_output.rfind("In [")]
+        splitted_output = stdout_output.split("\n")
+        for i, line in enumerate(splitted_output):
+            if not line[line.rfind(':')+1:].strip():
+                splitted_output[i] = ''
+            elif 'Out' in line:
+                splitted_output[i] = line[line.find(':')+2:]
+    return '\n'.join([line for line in splitted_output if line])
