3071322efd5c133866fae034400debc74ff8a7ef
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-08 21:40:40 +0800

    Update academic_search.py

--- academic_search.py
+++ academic_search.py
@@ -1,12 +1,34 @@
-# arxiv
 from langchain_community.retrievers import ArxivRetriever
-retriever = ArxivRetriever()
-def search_arxiv(query: str):
-    docs = retriever.invoke(query)
-    return [(doc.metadata['Title'], doc.page_content.replace('\n', ' '), doc.metadata['Entry ID']) for doc in docs]
-# crossref
 from habanero import Crossref
-cr = Crossref()
-def search_crossref(query: str):
-    x = cr.works(query = query)
-    return [(item["title"], item["abstract"], item["URL"]) for item in x['message']['items']]
+from typing import Tuple, List
+import re
+
+retriever = ArxivRetriever()
+cr = Crossref(timeout=30)
+CROSSREF_ABSTRACT_PATTERN = re.compile(r"<jats:p>(.*?)</jats:p>")
+
+
+def search_arxiv(query: str) -> List[Tuple[str, str, str]]:
+    docs = retriever.invoke(query)
+    return [
+        (
+            doc.metadata["Title"],
+            doc.page_content.replace("\n", " "),
+            doc.metadata["Entry ID"],
+        )
+        for doc in docs
+    ]
+
+
+def search_crossref(query: str) -> List[Tuple[str, str, str]]:
+    x = cr.works(query=query)
+    return [
+        (
+            item.get("title", "")[0],
+            CROSSREF_ABSTRACT_PATTERN.search(
+                item.get("abstract", "<jats:p></jats:p>")
+            ).group(1),
+            item.get("URL", ""),
+        )
+        for item in x["message"]["items"]
+    ]
