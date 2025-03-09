ba4febebcf63027b7c16b8282bd42c1b0a1563ff
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-09 16:58:53 +0800

    Update execute_code.py

--- execute_code.py
+++ execute_code.py
@@ -1,6 +1,5 @@
 import subprocess
 import re
-from langchain_core.tools import tool
 
 CLEAN_LINE_PATTERN = re.compile(r"^In \[\d+\]:\s*Out\[\d+\]:\s*")
 
@@ -18,14 +17,14 @@ def execute_code(code: str) -> str:
     output_blocks = []
 
     if (index := stdout_output.find("In [1]")) != -1:
-        stdout_output = stdout_output[index:stdout_output.rfind("In [")]
+        stdout_output = stdout_output[index : stdout_output.rfind("In [")]
         splitted_output = stdout_output.split("\n")
         for i, line in enumerate(splitted_output):
-            if not line[line.rfind(':')+1:].strip():
-                splitted_output[i] = ''
-            elif 'Out' in line:
-                splitted_output[i] = line[line.find(':')+2:]
-        cleaned_output = '\n'.join([line for line in splitted_output if line])
+            if not line[line.rfind(":") + 1 :].strip():
+                splitted_output[i] = ""
+            elif "Out" in line:
+                splitted_output[i] = line[line.find(":") + 2 :]
+        cleaned_output = "\n".join([line for line in splitted_output if line])
 
         code_block = f"```python\n{code}\n```"
         output_blocks.append(code_block)
@@ -35,9 +34,7 @@ def execute_code(code: str) -> str:
 
     return f"\n\n{'\n'.join(output_blocks)}\n"
 
-@tool
-def python(code: str) -> str:
-    """Ipython cell. Use numpy, scipy, sympy for numeric and symbolic computation."""
+def python_tool(code: str) -> str:
     process = subprocess.Popen(
         ["ipython"],
         stdin=subprocess.PIPE,
@@ -47,11 +44,13 @@ def python(code: str) -> str:
     )
     stdout_output, _ = process.communicate(code)
     if (index := stdout_output.find("In [1]")) != -1:
-        stdout_output = stdout_output[index:stdout_output.rfind("In [")]
+        stdout_output = stdout_output[index : stdout_output.rfind("In [")]
         splitted_output = stdout_output.split("\n")
         for i, line in enumerate(splitted_output):
-            if not line[line.rfind(':')+1:].strip():
-                splitted_output[i] = ''
-            elif 'Out' in line:
-                splitted_output[i] = line[line.find(':')+2:]
-    return '\n'.join([line for line in splitted_output if line])
+            if not line[line.rfind(":") + 1 :].strip():
+                splitted_output[i] = ""
+            elif "Out" in line:
+                splitted_output[i] = line[line.find(":") + 2 :]
+        cleaned_output = "\n".join([line for line in splitted_output if line])
+        return f'\n```\n{cleaned_output}\n```\n'
+    return ""
