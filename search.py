a80a074d729f5dcc662a8a56163ce74ec2ae5f27
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-09 16:59:43 +0800

    Update search.py

--- search.py
+++ search.py
@@ -3,7 +3,7 @@ from searXNG import searxng_websearch
 
 def attach_web_result(query: str) -> str:
     results = searxng_websearch(query)
-    return "\n\n".join(
+    return f'{"\n\n".join(
         f"# {i}. {title}\n{snippet}"
         for i, (title, snippet, _) in enumerate(results, start=1)
-    )
+    )}\n'
