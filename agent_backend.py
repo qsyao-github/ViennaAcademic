f1205bb7c30e549825cb9a80268080e229950e4d
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-12 22:43:48 +0800

    Update agent_backend.py

--- agent_backend.py
+++ agent_backend.py
@@ -1,22 +1,26 @@
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import StateGraph
-from langgraph.graph.message import BaseMessage
-from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.prebuilt.chat_agent_executor import AgentState
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from system_prompt import REGEX_TOOLCALL, WEB_SEARCH, KNOWLEDGEBASE
-from execute_code import python_tool
-from search import attach_web_result
-from langchain_core.tools import tool
+from typing import Dict, List
+
 from langchain_core.messages import (
-    HumanMessage,
     AIMessage,
+    BaseMessage,
+    HumanMessage,
     SystemMessage,
     convert_to_messages,
     merge_message_runs,
 )
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompt_values import PromptValue
+from langchain_core.tools import tool
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph
+from langgraph.graph.message import BaseMessage
+from langgraph.prebuilt import ToolNode, tools_condition, create_react_agent
+from langgraph.prebuilt.chat_agent_executor import AgentState
 
+from execute_code import python_tool
 from modelclient import pixtral_large_latest, deepseek_v3
+from search import attach_web_result
+from system_prompt import REGEX_TOOLCALL, WEB_SEARCH, KNOWLEDGEBASE
 
 regex_toolcall_template = ChatPromptTemplate.from_messages(
     [("system", REGEX_TOOLCALL), MessagesPlaceholder(variable_name="messages")]
@@ -39,15 +43,20 @@ select_template_from_mode = {
 
 
 class ChatAgentState(AgentState):
+    """聊天React Agent状态"""
+
     mode: str
+    """聊天模式：常规，多模态，知识库，网页搜索"""
+
     now_time: str
+    """当前时间戳，格式为%y%m%d%H%M%S，用于常规模式模型生成图片"""
 
 
 @tool
 def websearch(query: str) -> str:
     """使用搜索引擎"""
     result = attach_web_result(query)
-    return f'```\n搜索引擎前10结果：\n{result[0]}\n```\n\n参考文献\n\n{result[1]}\n\n'
+    return f"```\n搜索引擎前10结果：\n{result[0]}\n```\n\n参考文献\n\n{result[1]}\n\n"
 
 
 @tool
@@ -67,8 +76,16 @@ select_model_from_mode = {
 }
 
 
-# from langchain_core.messages import filter_messages
-def filter_tools(messages):
+# 以下两个函数参考langchain_core.messages.filter_messages
+def filter_tools(messages: PromptValue) -> List[BaseMessage]:
+    """过滤messages中的工具调用部分，包括AIMessage的工具调用信息和ToolMessage
+
+    Args:
+        messages: 待过滤的消息列表，由ChatPromptTemplate生成
+
+    Returns:
+        过滤后的消息列表
+    """
     messages = convert_to_messages(messages)
     filtered: list[BaseMessage] = []
     for message in messages:
@@ -82,11 +99,20 @@ def filter_tools(messages):
     return filtered
 
 
-def filter_multimodal(messages):
+def filter_multimodal(messages: PromptValue) -> List[BaseMessage]:
+    """过滤messages中的多模态部分，目前只有HumanMessage有可能出现多模态内容
+
+    Args:
+        messages: 待过滤的消息列表，由ChatPromptTemplate生成
+
+    Returns:
+        过滤后的消息列表
+    """
     messages = convert_to_messages(messages)
     filtered: list[BaseMessage] = []
     for message in messages:
         if isinstance(message, HumanMessage):
+            # 处理用户仅上传图片，不输入文字的情况
             filtered.append(
                 HumanMessage(
                     content=[
@@ -105,13 +131,23 @@ def filter_multimodal(messages):
     return filtered
 
 
-def chatbot(state: ChatAgentState):
+def chatbot(state: ChatAgentState) -> Dict[str, List[BaseMessage]]:
+    """进行一轮React Agent推理
+
+    Args:
+        state: 当前状态
+
+    Returns:
+        一轮推理后的状态。对于messages，langchain实现了reducer函数，信息默认附加在上一个状态后
+    """
     mode = state["mode"]
     template = select_template_from_mode[mode]
     model = select_model_from_mode[mode]
     prompted_message = template.invoke(state)
+    # 仅多模态模式的模型支持多模态信息
     if mode != "多模态":
         prompted_message = filter_multimodal(prompted_message)
+    # 仅常规模式的模型支持工具信息
     if mode != "常规":
         prompted_message = filter_tools(prompted_message)
     merged = merge_message_runs(prompted_message)
@@ -119,13 +155,9 @@ def chatbot(state: ChatAgentState):
     return {"messages": [response]}
 
 
-"""AIMessage(content='', additional_kwargs={'tool_calls': [{'index': 0, 'id': 'call_a62zpuxfjk0phwgz2m3382mx', 'function': {'arguments': '{"code":"import numpy as np\\nimport matplotlib.pyplot as plt\\n\\nx = np.linspace(0, 2 * np.pi, 1000)\\ny = np.sin(x)\\n\\nplt.plot(x, y)\\nplt.title(\'Plot of sin(x)\')\\nplt.xlabel(\'x\')\\nplt.ylabel(\'sin(x)\')\\nplt.grid(True)\\nplt.savefig(\'250311130554.png\')\\nplt.show()"}', 'name': 'ipython'}, 'type': 'function'}]}, response_metadata={'finish_reason': 'tool_calls', 'model_name': 'deepseek-v3-241226'}, id='run-b6b04698-3d18-43fc-be98-38d0b284bbd5', tool_calls=[{'name': 'ipython', 'args': {'code': "import numpy as np\nimport matplotlib.pyplot as plt\n\nx = np.linspace(0, 2 * np.pi, 1000)\ny = np.sin(x)\n\nplt.plot(x, y)\nplt.title('Plot of sin(x)')\nplt.xlabel('x')\nplt.ylabel('sin(x)')\nplt.grid(True)\nplt.savefig('250311130554.png')\nplt.show()"}, 'id': 'call_a62zpuxfjk0phwgz2m3382mx', 'type': 'tool_call'}])"""
-
 graph_builder.add_node("chatbot", chatbot)
-
 tool_node = ToolNode(tools=tools)
 graph_builder.add_node("tools", tool_node)
-
 graph_builder.add_conditional_edges(
     "chatbot",
     tools_condition,
