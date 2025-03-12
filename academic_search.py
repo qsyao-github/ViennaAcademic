5e17ea10f21c76c5e230a9adc946f3133a1c1b09
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-12 22:43:20 +0800

    Update academic_search.py

--- academic_search.py
+++ academic_search.py
@@ -1,11 +1,27 @@
-from langchain_community.retrievers import ArxivRetriever
 from typing import Tuple, List
+
+from langchain_community.retrievers import ArxivRetriever
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_community.vectorstores.utils import DistanceStrategy
+from langchain.retrievers import ContextualCompressionRetriever
+
+from custom_reranker import CustomCompressor
+from modelclient import bce_embedding_base
 from searXNG import searxng_academic_search
 
 retriever = ArxivRetriever()
+reranker = CustomCompressor(10)
 
 
 def search_arxiv(query: str) -> List[Tuple[str, str, str]]:
+    """搜索arxiv论文，返回标题、摘要和链接
+
+    Args:
+        query: Arxiv ID或搜索关键词
+
+    Returns:
+        (标题, 摘要, 链接)元组组成的列表
+    """
     docs = retriever.invoke(query)
     return [
         (
@@ -17,17 +33,15 @@ def search_arxiv(query: str) -> List[Tuple[str, str, str]]:
     ]
 
 
-from modelclient import bce_embedding_base
-from typing import List
-from custom_reranker import CustomCompressor
-from langchain_community.vectorstores.faiss import FAISS
-from langchain_community.vectorstores.utils import DistanceStrategy
-from langchain.retrievers import ContextualCompressionRetriever
+def select_academic_search_result(query: str) -> List[Tuple[str, str, str]]:
+    """使用searxng获取学术搜索结果，并使用bce embedding+rerank召回相关性>=0.35的前十结果
 
-reranker = CustomCompressor(10)
+    Args:
+        query: 搜索关键词
 
-
-def select_best_results(query: str) -> List[Tuple[str, str, str]]:
+    Returns:
+        (标题, 摘要, 链接)元组组成的列表
+    """
     results = searxng_academic_search(query)
     texts = [f"{item[0]}\n{item[1]}"[:512] for item in results]
     retriever = FAISS.from_texts(
