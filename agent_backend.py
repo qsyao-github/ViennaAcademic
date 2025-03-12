3d8882276353a85bfb94dcc00e4ad93914ba9bd3
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-12 20:25:40 +0800

    Update agent_backend.py

--- agent_backend.py
+++ agent_backend.py
@@ -1,7 +1,8 @@
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import StateGraph, MessagesState
-from langgraph.graph.message import add_messages, BaseMessage
+from langgraph.graph import StateGraph
+from langgraph.graph.message import BaseMessage
 from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.prebuilt.chat_agent_executor import AgentState
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from system_prompt import REGEX_TOOLCALL, WEB_SEARCH, KNOWLEDGEBASE
 from execute_code import python_tool
@@ -11,20 +12,11 @@ from langchain_core.messages import (
     HumanMessage,
     AIMessage,
     SystemMessage,
-    filter_messages,
     convert_to_messages,
     merge_message_runs,
 )
 
-from modelclient import (
-    gpt_4o_mini,
-    glm_4_flash,
-    pixtral_large_latest,
-    deepseek_v3,
-    deepseek_r1_671b,
-    deepseek_r1_70b,
-    qwq_32b,
-)
+from modelclient import pixtral_large_latest, deepseek_v3
 
 regex_toolcall_template = ChatPromptTemplate.from_messages(
     [("system", REGEX_TOOLCALL), MessagesPlaceholder(variable_name="messages")]
@@ -46,7 +38,7 @@ select_template_from_mode = {
 }
 
 
-class ChatMessageState(MessagesState):
+class ChatAgentState(AgentState):
     mode: str
     now_time: str
 
@@ -54,7 +46,8 @@ class ChatMessageState(MessagesState):
 @tool
 def websearch(query: str) -> str:
     """使用搜索引擎"""
-    return attach_web_result(query)
+    result = attach_web_result(query)
+    return f'```\n搜索引擎前10结果：\n{result[0]}\n```\n\n参考文献\n\n{result[1]}\n\n'
 
 
 @tool
@@ -63,7 +56,7 @@ def ipython(code: str) -> str:
     return python_tool(code)
 
 
-graph_builder = StateGraph(ChatMessageState)
+graph_builder = StateGraph(ChatAgentState)
 tools = [ipython, websearch]
 deepseek_v3_with_tools = deepseek_v3.bind_tools(tools)
 select_model_from_mode = {
@@ -72,6 +65,7 @@ select_model_from_mode = {
     "知识库": deepseek_v3,
     "网页搜索": deepseek_v3,
 }
+
 
 # from langchain_core.messages import filter_messages
 def filter_tools(messages):
@@ -97,15 +91,21 @@ def filter_multimodal(messages):
                 HumanMessage(
                     content=[
                         chunk for chunk in message.content if chunk["type"] == "text"
-                    ] or [{'type': 'text', 'text': '系统提示：用户没有输入任何内容，请礼貌的要求用户输入文本或使用多模态模式'}]
+                    ]
+                    or [
+                        {
+                            "type": "text",
+                            "text": "系统提示：用户没有输入任何内容，请礼貌的要求用户输入文本或使用多模态模式",
+                        }
+                    ]
                 )
             )
-        elif isinstance(message, AIMessage) or isinstance(message, SystemMessage):
+        else:
             filtered.append(message)
     return filtered
 
 
-def chatbot(state: ChatMessageState):
+def chatbot(state: ChatAgentState):
     mode = state["mode"]
     template = select_template_from_mode[mode]
     model = select_model_from_mode[mode]
@@ -116,7 +116,7 @@ def chatbot(state: ChatMessageState):
         prompted_message = filter_tools(prompted_message)
     merged = merge_message_runs(prompted_message)
     response = model.invoke(merged)
-    return {"messages": response}
+    return {"messages": [response]}
 
 
 """AIMessage(content='', additional_kwargs={'tool_calls': [{'index': 0, 'id': 'call_a62zpuxfjk0phwgz2m3382mx', 'function': {'arguments': '{"code":"import numpy as np\\nimport matplotlib.pyplot as plt\\n\\nx = np.linspace(0, 2 * np.pi, 1000)\\ny = np.sin(x)\\n\\nplt.plot(x, y)\\nplt.title(\'Plot of sin(x)\')\\nplt.xlabel(\'x\')\\nplt.ylabel(\'sin(x)\')\\nplt.grid(True)\\nplt.savefig(\'250311130554.png\')\\nplt.show()"}', 'name': 'ipython'}, 'type': 'function'}]}, response_metadata={'finish_reason': 'tool_calls', 'model_name': 'deepseek-v3-241226'}, id='run-b6b04698-3d18-43fc-be98-38d0b284bbd5', tool_calls=[{'name': 'ipython', 'args': {'code': "import numpy as np\nimport matplotlib.pyplot as plt\n\nx = np.linspace(0, 2 * np.pi, 1000)\ny = np.sin(x)\n\nplt.plot(x, y)\nplt.title('Plot of sin(x)')\nplt.xlabel('x')\nplt.ylabel('sin(x)')\nplt.grid(True)\nplt.savefig('250311130554.png')\nplt.show()"}, 'id': 'call_a62zpuxfjk0phwgz2m3382mx', 'type': 'tool_call'}])"""
