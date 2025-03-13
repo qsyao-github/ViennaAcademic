820454c00499c889a815e7bdf5775c753623c120
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-13 19:18:57 +0800

    Create modelclient.py

---
+++ modelclient.py
@@ -0,0 +1,64 @@
+from api_keys import (
+    silicon_client_API_KEY,
+    silicon_client_BASE_URL,
+    volcano_client_API_KEY,
+    volcano_client_BASE_URL,
+    xkx_client_API_KEY,
+    xkx_client_BASE_URL,
+    zhipu_client_API_KEY,
+    zhipu_client_BASE_URL,
+)
+from langchain_deepseek import ChatDeepSeek
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+# 文生文
+deepseek_v3 = ChatOpenAI(
+    model="deepseek-v3-241226",
+    api_key=volcano_client_API_KEY,
+    base_url=volcano_client_BASE_URL,
+)
+
+# 多模态
+pixtral_large_latest = ChatOpenAI(
+    model="pixtral-large-latest",
+    api_key=xkx_client_API_KEY,
+    base_url=xkx_client_BASE_URL,
+)
+
+glm_4v_flash = ChatOpenAI(
+    model="glm-4v-flash",
+    api_key=zhipu_client_API_KEY,
+    base_url=zhipu_client_BASE_URL,
+)
+
+# 推理
+# ChatDeepSeek中base_url疑似没有作用，必须使用api_base
+deepseek_r1_671b = ChatDeepSeek(
+    model="deepseek-r1-250120",
+    api_key=volcano_client_API_KEY,
+    api_base=volcano_client_BASE_URL,
+    temperature=0.6,
+    max_tokens=16384,
+)
+
+qwq_32b = ChatOpenAI(
+    model="qwen-qwq-32b",
+    api_key=xkx_client_API_KEY,
+    base_url=xkx_client_BASE_URL,
+    temperature=0.6,
+    top_p=0.95,
+)
+
+# 代码
+codestral_latest = ChatOpenAI(
+    model="codestral-latest",
+    api_key=xkx_client_API_KEY,
+    base_url=xkx_client_BASE_URL,
+)
+
+# 嵌入
+bce_embedding_base = OpenAIEmbeddings(
+    model="netease-youdao/bce-embedding-base_v1",
+    api_key=silicon_client_API_KEY,
+    base_url=silicon_client_BASE_URL,
+)
