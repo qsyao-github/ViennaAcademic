7a4f2a0b5b1a70e455abd263b08cb93e7227d21a
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-15 17:38:20 +0800

    Update docling_parser.py

--- docling_parser.py
+++ docling_parser.py
@@ -1,25 +1,31 @@
+"""
+docling的接口，加入自定义设置，提高解析精度
+"""
+import re
+
+from docling.backend.docling_parse_v2_backend import DoclingParseV2DocumentBackend
 from docling.datamodel.base_models import InputFormat
-from docling.document_converter import (
-    DocumentConverter,
-    PdfFormatOption,
-    WordFormatOption,
-    ImageFormatOption,
-)
-from docling.pipeline.simple_pipeline import SimplePipeline
+from docling.datamodel.document import ConversionResult
 from docling.datamodel.pipeline_options import (
+    AcceleratorDevice,
+    AcceleratorOptions,
     PdfPipelineOptions,
     RapidOcrOptions,
     TableFormerMode,
-    AcceleratorDevice,
-    AcceleratorOptions,
 )
-from docling.backend.docling_parse_v2_backend import DoclingParseV2DocumentBackend
-from docling.datamodel.document import ConversionResult
+from docling.document_converter import (
+    DocumentConverter,
+    ImageFormatOption,
+    PdfFormatOption,
+    WordFormatOption,
+)
+from docling.pipeline.simple_pipeline import SimplePipeline
 from rapidocr_onnxruntime import RapidOCR
-import re
 
 engine = RapidOCR()
-pipeline_options = PdfPipelineOptions(artifacts_path="/home/laowei/model/ds4sd/docling-models")
+pipeline_options = PdfPipelineOptions(
+    artifacts_path="/home/laowei/model/docling-models"
+)
 pipeline_options.do_ocr = True
 pipeline_options.accelerator_options = AcceleratorOptions(
     num_threads=20, device=AcceleratorDevice.CPU
@@ -49,24 +55,52 @@ doc_converter = DocumentConverter(  # all of the below is optional, has internal
     },
 )
 
-
-def parse_everything(file_or_url: str) -> str:
-    conv_result: ConversionResult = doc_converter.convert(file_or_url)
-    result = conv_result.document.export_to_markdown()
-    return result.replace("<!-- image -->", "").strip()
-
-
+"""去除arxiv html解析结果后的无用信息"""
 ARXIV_STRIP_PATTERN = re.compile(
     r"(.*?)(?=Generated  on \w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4} by)", re.DOTALL
 )
 
 
+def parse_everything(file_or_url: str) -> str:
+    """docling的万能接口
+    
+    支持pdf, docx, pptx, html, image。文档解析结果中图片用<!-- image -->表示，予以去除
+
+    Parameters
+    ----------
+    file_or_url: str
+        文件路径或url
+
+    Returns
+    ----------
+    str
+        markdown解析结果
+    """
+    conv_result: ConversionResult = doc_converter.convert(file_or_url)
+    result = conv_result.document.export_to_markdown()
+    return result.replace("<!-- image -->", "").strip()
+
+
 def parse_arxiv(url: str) -> str:
+    """解析arxiv论文html
+    
+    有结果清洗机制和异常处理机制。解析失败返回空字符串，方便arxiv模块的函数处理该错误。
+
+    Parameters
+    ----------
+    url: str
+        arxiv论文html的url
+
+    Returns
+    ----------
+    str
+        markdown解析结果
+    """
     try:
         text = parse_everything(url)
         match = ARXIV_STRIP_PATTERN.search(text)
         if match is not None:
             return match.group(1).strip()
-    except:
-        pass
+    except Exception as e:
+        print(f"An error occurred: {e}")
     return ""
