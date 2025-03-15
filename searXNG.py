c42767a07522c550abcf58a510908f03a7a24f89
qsyao-github <55919774+qsyao-github@users.noreply.github.com>
2025-03-15 17:25:49 +0800

    Update searXNG.py

--- searXNG.py
+++ searXNG.py
@@ -1,10 +1,36 @@
-from langchain_community.utilities import SearxSearchWrapper
-from typing import Tuple, List
-import re
+"""
+SearXNG搜索引擎接口
+"""
 
-academic_search_wrapper = SearxSearchWrapper(searx_host="http://localhost:8080", k=256)
+import re
+from typing import List, Tuple
+
+from langchain_community.utilities import SearxSearchWrapper
+
+"""正则表达式，用于去除html标签"""
+REMOVE_HTML_PATTERN = re.compile(r"<[^>]+>")
+"""最多返回结果数。embedding模型召回100个文段，据此选择256"""
+MAX_RESULTS = 256
+academic_search_wrapper = SearxSearchWrapper(
+    searx_host="http://localhost:8080", k=MAX_RESULTS
+)
+
 
 def searxng_websearch(query: str) -> List[Tuple[str, str, str]]:
+    """SearXNG网页搜索
+
+    仅返回前10结果
+
+    Parameters
+    ----------
+    query: str
+        搜索关键词
+
+    Returns
+    ----------
+    List[Tuple[str, str, str]]
+        搜索结果，标题、摘要和链接的元组
+    """
     results = academic_search_wrapper.results(
         query,
         categories=["general"],
@@ -13,11 +39,22 @@ def searxng_websearch(query: str) -> List[Tuple[str, str, str]]:
     )
     return [(result["title"], result["snippet"], result["link"]) for result in results]
 
-REMOVE_XML_PATTERN = re.compile(r'<[^>]+>')
 
-
-# 定义一个函数，用于进行学术搜索
 def searxng_academic_search(query: str) -> List[Tuple[str, str, str]]:
+    """SearXNG学术搜索
+
+    crossref搜索结果可能包含html标签，需要去除
+
+    Parameters
+    ----------
+    query: str
+        搜索关键词
+
+    Returns
+    ----------
+    List[Tuple[str, str, str]]
+        搜索结果，标题、摘要和链接的元组
+    """
     results = academic_search_wrapper.results(
         query,
         categories=["science"],
@@ -30,7 +67,14 @@ def searxng_academic_search(query: str) -> List[Tuple[str, str, str]]:
             "openairepublications",
             "pbde",
         ],
-        num_results=256,
+        num_results=MAX_RESULTS,
         language="all",
     )
-    return [(result["title"], REMOVE_XML_PATTERN.sub('', result["snippet"]), result["link"]) for result in results]
+    return [
+        (
+            result["title"],
+            REMOVE_HTML_PATTERN.sub("", result["snippet"]),
+            result["link"],
+        )
+        for result in results
+    ]
