20360e31db878957053325bc001c889097678346
qsyao-github <1508179189@qq.com>
2025-05-24 10:48:07 +0800

    Update script updated

---
+++ update.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+mamba update --all -y
+mamba clean --all -f -y
+rustup self update
+rustup update stable
+cd va_rust_utils
+cargo upgrade -i
+cargo update
+cargo install-update -a
+cargo cache -a
+cd ../app
+pip3 install -U --upgrade-strategy eager --no-cache-dir -r requirements.txt
+pip cache purge
