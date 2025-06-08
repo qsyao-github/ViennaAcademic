0c49b736c0c868979aa727833444b7f6a9d15268
qsyao-github <1508179189@qq.com>
2025-06-08 21:34:59 +0800

    Recompile rust code after update

--- update.sh
+++ update.sh
@@ -9,6 +9,8 @@ cd va_rust_utils
 cargo upgrade -i
 cargo update
 cargo install-update -a
+cargo clean
+RUSTFLAGS='-C target-cpu=native' maturin develop
 cargo cache -a
 cd ../app
 pip3 install -U --upgrade-strategy eager --no-cache-dir torch torchvision torchaudio -f https://mirrors.aliyun.com/pytorch-wheels/cu128 && pip3 install -U --upgrade-strategy eager --no-cache-dir -r requirements.txt
